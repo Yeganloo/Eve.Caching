8a25854b565f5ac4e1e223dfbe3eefeaca62f228
Hosein Yeganloo <hoseinyeganloo@gmail.com>
2019-12-28 18:11:47 +0330

    Add simple cache test

---
+++ Eve.Caching.Redis/RedisCacheProvider.cs
@@ -0,0 +1,142 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Eve.Caching.Redis
+{
+    public class RedisCacheProvider<TVal> : ICacheProvider<string, TVal>
+    {
+        private ConnectionMultiplexer Connection;
+        private MessagePack.MessagePackSerializerOptions Option;
+
+        private byte[] Serialize(TVal obj)
+        {
+            return MessagePack.MessagePackSerializer.Serialize(obj, Option);
+        }
+        private T Deserialize<T>(byte[] val)
+        {
+            if (val == null)
+                return default(T);
+
+            return MessagePack.MessagePackSerializer.Deserialize<T>(val, Option);
+        }
+
+        private object Deserialize(byte[] val, Type type)
+        {
+            return typeof(MessagePack.MessagePackSerializer).GetMethod("Deserialize", new Type[] { typeof(byte[]), typeof(MessagePack.MessagePackSerializerOptions) }).MakeGenericMethod(type).Invoke(null, new object[] { val, Option });
+        }
+
+        public IDatabase _Cache
+        {
+            get
+            {
+                return Connection.GetDatabase();
+            }
+        }
+
+
+        public RedisCacheProvider(ConfigurationOptions options, MessagePack.MessagePackSerializerOptions mspOptions = null)
+        {
+            Option = mspOptions ?? MessagePack.Resolvers.ContractlessStandardResolver.Options;
+            Connection = ConnectionMultiplexer.Connect(options);
+        }
+
+        public TVal this[string key]
+        {
+            get => Deserialize<TVal>((byte[])_Cache.StringGet(key));
+            set
+            {
+                _Cache.StringSet(key, Serialize(value));
+            }
+        }
+
+        public TVal this[string key, string SubKey]
+        {
+            get => Deserialize<TVal>((byte[])_Cache.HashGet(key, SubKey));
+            set
+            {
+                _Cache.HashSet(key, SubKey, Serialize(value));
+            }
+        }
+
+        public void Cache(string name, TVal obj, TimeOutMode mode, int timeOut)
+        {
+            if (timeOut < 0)
+                throw new ArgumentException($"TimeOut is a Posetive number!\r\nValue is {timeOut}");
+            if ((mode & (TimeOutMode.FromCreate | TimeOutMode.Never)) == 0)
+                throw new ArgumentException($"TimeOut Mode is not supported!\r\nMode is {mode.ToString()}");
+            this[name] = obj;
+            if (mode == TimeOutMode.FromCreate)
+                _Cache.KeyExpireAsync(name, DateTime.Now.AddMilliseconds(timeOut));
+        }
+
+        public void Cache(string key, TVal obj)
+        {
+            this[key] = obj;
+        }
+
+        public void Cache(string key, string subkey, TVal obj)
+        {
+            this[key, subkey] = obj;
+        }
+
+        public void Cache(string key, string subkey, TVal obj, TimeOutMode mode, int timeOut)
+        {
+            if (timeOut < 0)
+                throw new ArgumentException($"TimeOut is a Posetive number!\r\nValue is {timeOut}");
+            if ((mode & (TimeOutMode.FromCreate | TimeOutMode.Never)) == 0)
+                throw new ArgumentException($"TimeOut Mode is not supported!\r\nMode is {mode.ToString()}");
+            this[key, subkey] = obj;
+            if (mode == TimeOutMode.FromCreate)
+                _Cache.KeyExpireAsync(key, DateTime.Now.AddMilliseconds(timeOut));
+        }
+
+        /// <summary>
+        /// To dangerous!
+        /// Clear Entire Database!
+        /// </summary>
+        public void Clear()
+        {
+            _Cache.Execute("FLUSHDB");
+        }
+
+        public bool HasKey(string key)
+        {
+            return _Cache.KeyExists(key);
+        }
+
+        public void Remove(string name)
+        {
+            _Cache.KeyDelete(name);
+        }
+
+        public void Remove(string key, string subKey)
+        {
+            _Cache.HashDelete(key, subKey);
+        }
+
+        public bool HasKey(string key, string subKey)
+        {
+            return _Cache.HashExists(key, subKey);
+        }
+
+        public T Get<T>(string key) where T : TVal
+        {
+            return Deserialize<T>((byte[])_Cache.StringGet(key));
+        }
+
+        public T Get<T>(string key, string SubKey) where T : TVal
+        {
+            return Deserialize<T>((byte[])_Cache.HashGet(key, SubKey));
+        }
+
+        public object Get(string key, Type type)
+        {
+            return Deserialize((byte[])_Cache.StringGet(key), type);
+        }
+
+        public object Get(string key, string SubKey, Type type)
+        {
+            return Deserialize((byte[])_Cache.HashGet(key, SubKey), type);
+        }
+    }
+}
---
+++ Eve.CachingTest/SimpleCache.cs
@@ -0,0 +1,127 @@
+using Eve.Caching;
+using System.Threading;
+using Xunit;
+
+namespace Eve.CachingTest
+{
+    public class SimpleCache
+    {
+        private const int _Rounds = 1000;
+        public class testObj
+        {
+            public int I;
+        }
+
+        public ICacheProvider<string, testObj> GetProvider()
+        {
+            return new SimpleCacheProvider<testObj>();
+        }
+
+
+        [Fact]
+        public void ReadWrite()
+        {
+            var cache = GetProvider();
+            var tmp = new testObj[_Rounds];
+            for (int i = 0; i < _Rounds; i++)
+            {
+                cache.Cache(i.ToString(), tmp[i] = new testObj { I = i });
+            }
+            for (int i = 0; i < _Rounds; i++)
+            {
+                Assert.Equal(tmp[i], cache.Get<testObj>(i.ToString()));
+            }
+        }
+
+        [Fact]
+        public void Remove()
+        {
+            var cache = GetProvider();
+            cache.Cache("tst", new testObj());
+            cache.Remove("tst");
+            Assert.Null(cache.Get<testObj>("tst"));
+        }
+
+        [Fact]
+        public void HasKey()
+        {
+            var cache = GetProvider();
+            cache.Cache("tst", new testObj());
+            Assert.True(cache.HasKey("tst"));
+            cache.Remove("tst");
+            Assert.False(cache.HasKey("tst"));
+        }
+
+        [Fact]
+        public void ExpierAccess()
+        {
+            var cache = GetProvider();
+            cache.Cache("tst", new testObj(), TimeOutMode.LastUse, 1);
+            Thread.Sleep(900);
+            Assert.NotNull(cache.Get<testObj>("tst"));
+            Thread.Sleep(900);
+            Assert.NotNull(cache.Get<testObj>("tst"));
+            Thread.Sleep(1001);
+            Assert.Null(cache.Get<testObj>("tst"));
+        }
+
+        [Fact]
+        public void ExpierCnt()
+        {
+            var cache = GetProvider();
+            cache.Cache("tst", new testObj(), TimeOutMode.AccessCount, 2);
+            Assert.NotNull(cache.Get<testObj>("tst"));
+            Assert.NotNull(cache.Get<testObj>("tst"));
+            Assert.Null(cache.Get<testObj>("tst"));
+        }
+
+        [Fact]
+        public void ExpierCreate()
+        {
+            var cache = GetProvider();
+            cache.Cache("tst", new testObj(), TimeOutMode.FromCreate, 1);
+            Thread.Sleep(500);
+            Assert.NotNull(cache.Get<testObj>("tst"));
+            Thread.Sleep(300);
+            Assert.NotNull(cache.Get<testObj>("tst"));
+            Thread.Sleep(300);
+            Assert.Null(cache.Get<testObj>("tst"));
+        }
+
+
+        [Fact]
+        public void ReadWrite_Subkey()
+        {
+            var cache = GetProvider();
+            var tmp = new testObj[_Rounds];
+            for (int i = 0; i < _Rounds; i++)
+            {
+                cache.Cache("rw", i.ToString(), tmp[i] = new testObj { I = i });
+            }
+            for (int i = 0; i < _Rounds; i++)
+            {
+                Assert.Equal(tmp[i], cache.Get<testObj>("rw", i.ToString()));
+            }
+        }
+
+        [Fact]
+        public void Remove_Subkey()
+        {
+            var cache = GetProvider();
+            cache.Cache("rm", "tst", new testObj());
+            cache.Remove("rm", "tst");
+            Assert.Null(cache.Get<testObj>("rm", "tst"));
+        }
+
+        [Fact]
+        public void HasKey_Subkey()
+        {
+            var cache = GetProvider();
+            cache.Cache("hs", "tst", new testObj());
+            Assert.True(cache.HasKey("hs", "tst"));
+            cache.Remove("hs", "tst");
+            Assert.False(cache.HasKey("hs", "tst"));
+        }
+
+    }
+}
